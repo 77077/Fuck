3f01aa8e611bfda33138bafba072768d206e5296
77077 <81890@163.com>
2017-09-12 22:46:37 +0800

    2.0

--- src/FuckSeven.java
+++ src/FuckSeven.java
@@ -1,7 +1,7 @@
 
 public class FuckSeven {
-public static void main(String[] args) {
-	System.out.println("fucksevfgfdhsjsen");
-	
-}
+	public static void main(String[] args) {
+		System.out.println("fucksevfgfdhsjsen");
+		int t = 200;
+	}
 }
